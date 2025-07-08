a1045f2304cb1a87cfb968903bb08f302e63e706
unknown <rolando@email.com>
2025-07-08 13:02:50 -0500

    home estilo

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),  # Nueva ruta raíz
     path('admin/', admin.site.urls),
     path('api/estudiantes/', include('estudiantes.urls')),
     path('api/medical/', include('medical.urls')),
     path('api/reportes/', include('reportes.urls')),
 ]
+
+
